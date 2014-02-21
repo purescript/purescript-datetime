226a0b6fd82184e824f22a1e30780a1150f06acc
Gary Burgess <gary.burgess@gmail.com>
2014-02-21 21:12:28 +0000

    Update prelude imports

--- examples/Date.purs
+++ examples/Date.purs
@@ -1,44 +1,47 @@
 module Main where
 
-  import Prelude
-  import Maybe
-  import Eff
-  import Trace
-  import Data.Date
-  
-  foreign import testJSDateOk "var testJSDateOk = new Date();" :: JSDate
-  foreign import testJSDateBad "var testJSDateBad = new Date('fail');" :: JSDate
+import Prelude
+import Data.Date
+import Data.Maybe
+import Control.Monad.Eff
+import Debug.Trace
 
-  main = do
-    d <- now
-    Trace.print d
-    
-    Trace.print $ fromJSDate testJSDateOk
-    Trace.print $ fromJSDate testJSDateBad
-    
-    Trace.print $ fromString "Tue Feb 18 2014 23:24:53 GMT+3000"
-    Trace.print $ date 2003 January 01
-    Trace.print $ dateTime 2044 February 01 13 26 48 06
-    
-    Trace.print $ toYear d
-    Trace.print $ toMonth d
-    Trace.print $ toDay d
-    Trace.print $ toHours d
-    Trace.print $ toMinutes d
-    Trace.print $ toSeconds d
-    Trace.print $ toMilliseconds d
-    
-    Trace.print $ toUTCYear d
-    Trace.print $ toUTCMonth d
-    Trace.print $ toUTCDay d
-    Trace.print $ toUTCHours d
-    Trace.print $ toUTCMinutes d
-    Trace.print $ toUTCSeconds d
-    Trace.print $ toUTCMilliseconds d
-    
-    Trace.print $ timezoneOffset d
-    Trace.print $ toEpochMilliseconds d
-    
-    Trace.print $ fromEpochMilliseconds 0
-    Trace.print $ fromEpochMilliseconds 999999999999
-    Trace.print $ fromEpochMilliseconds (0-999999999999)
+foreign import testJSDateOk "var testJSDateOk = new Date();" :: JSDate
+foreign import testJSDateBad "var testJSDateBad = new Date('fail');" :: JSDate
+
+main = do
+  d <- now
+  print d
+  
+  print $ fromJSDate testJSDateOk
+  print $ fromJSDate testJSDateBad
+  
+  print $ fromString "Tue Feb 18 2014 23:24:53 GMT+3000"
+  print $ date 2003 January 01
+  print $ dateTime 2044 February 01 13 26 48 06
+  
+  print $ toYear d
+  print $ toMonth d
+  print $ toDay d
+  print $ toHours d
+  print $ toMinutes d
+  print $ toSeconds d
+  print $ toMilliseconds d
+  
+  print $ toUTCYear d
+  print $ toUTCMonth d
+  print $ toUTCDay d
+  print $ toUTCHours d
+  print $ toUTCMinutes d
+  print $ toUTCSeconds d
+  print $ toUTCMilliseconds d
+  
+  print $ timezoneOffset d
+  print $ toEpochMilliseconds d
+  
+  print $ fromEpochMilliseconds 0
+  print $ fromEpochMilliseconds 999999999999
+  print $ fromEpochMilliseconds (0-999999999999)
+  
+  print $ (unsafeFromJust (date 2003 January 01)) < d
+  print $ d == d
