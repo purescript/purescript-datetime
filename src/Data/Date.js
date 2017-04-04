658e64cfa405c922e53f1979e5b8b9e4c7ff7c56
Anil Anar <anilanar@hotmail.com>
2017-04-04 23:41:47 +0200

    fix date/instant construction for years 0 <= y < 100

--- src/Data/Date.js
+++ src/Data/Date.js
@@ -1,16 +1,24 @@
 "use strict";
 
+var createDate = function (y, m, d) {
+  var date = new Date(Date.UTC(y, m, d));
+  if (y >= 0 && y < 100) {
+    date.setUTCFullYear(y);
+  }
+  return date;
+};
+
 exports.canonicalDateImpl = function (ctor, y, m, d) {
-  var date = new Date(Date.UTC(y, m - 1, d));
+  var date = createDate(y, m - 1, d);
   return ctor(date.getUTCFullYear())(date.getUTCMonth() + 1)(date.getUTCDate());
 };
 
 exports.calcWeekday = function (y, m, d) {
-  return new Date(Date.UTC(y, m - 1, d)).getUTCDay();
+  return createDate(y, m - 1, d).getUTCDay();
 };
 
 exports.calcDiff = function (y1, m1, d1, y2, m2, d2) {
-  var dt1 = new Date(Date.UTC(y1, m1 - 1, d1));
-  var dt2 = new Date(Date.UTC(y2, m2 - 1, d2));
+  var dt1 = createDate(y1, m1 - 1, d1);
+  var dt2 = createDate(y2, m2 - 1, d2);
   return dt1.getTime() - dt2.getTime();
 };
